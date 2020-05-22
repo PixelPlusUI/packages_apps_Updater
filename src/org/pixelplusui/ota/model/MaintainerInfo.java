9c55c4ee18dc563b7643cf2bbcd3d1a511c35c8d
KostyaJRZ <jrzroyale@gmail.com>
2020-05-23 01:48:19 +0530

    Updater: Initalize PixelPlusUI Updater

---
+++ src/org/pixelplusui/ota/model/MaintainerInfo.java
@@ -0,0 +1,14 @@
+package org.pixelplusui.ota.model;
+
+public class MaintainerInfo {
+    private String name;
+    private String github_username;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return github_username;
+    }
+}
